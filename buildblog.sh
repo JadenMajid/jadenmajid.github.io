f5b9915098e9f9f312082a4c2cc058c23959f53d
Jaden Majid <majid.jaden@gmail.com>
2026-04-10 16:37:10 -0700

    removed jekyll build artefacts

--- buildblog.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# buildblog.sh: Build the Jekyll site.
-
-echo "Building Jekyll site..."
-bundle exec jekyll build
--- watchblog.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# watchblog.sh: Serve the Jekyll site with live reload.
-
-echo "Starting Jekyll server..."
-bundle exec jekyll serve --livereload
